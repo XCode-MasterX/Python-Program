f35edda377b4299bb7b8f12224897ca61024beb0
XCode-MasterX <arpan1.dash@gmail.com>
2022-03-28 12:12:32 +0530

    commit

---
+++ School Project/student management.py
@@ -0,0 +1,69 @@
+import mysql.connector as msc
+
+# GLOBAL #
+con = msc.connect(user = 'root', host = 'localhost', password = '1Ar2pan34')
+cur = con.cursor()
+# END #
+
+def exec(command):
+    cur.execute(command);
+
+def update_all_student_grades(connection):
+    connection.cursor().execute("update students set class = class + 1;");
+
+def create_id(student_info):
+    id = str(student_info['mobile'][0:5])
+    id += str(student_info['firstname'])
+    student_info['unique_id'] = id;
+
+def admit_new_student():
+    stud = {}
+    stud['firstname'] = input('Enter the firstname of student: ')
+    stud['lastname'] = input('Enter the lastname of the student')
+    stud['f-name'] = input('Enter the fathers name: ')
+    stud['m-name'] = input('Enter the mothers name: ')
+    stud['mobile'] = input('Enter the mobile number: ')
+    stud['dob'] = input('Enter the date of birth: ')
+    stud['class'] = input('Enter the class of the student: ')
+    stud['section'] = input('Enter the section of the student: ')
+    stud['address'] = input('Enter the address: ')
+    stud['pin'] = input('Enter the pin of the address: ')
+    create_id(stud);
+
+def update_student(id):
+    pass
+
+def check_database_existance():
+    exec('show databases;')
+    databases = [i[0] for i in cur.fetchall()]
+    if 'institution' not in databases:
+        exec("create database Institution;")
+    exec('use Institution;');
+
+def display_students():
+    pass
+
+def check_table_existance():
+    exec('use Institution;')
+    exec('show tables;')
+    if cur.fetchall() == []:
+        exec('''create table Students
+                (student_id varchar(10) PRIMARY KEY,
+                firstname varchar(15) NOT NULL,
+                lastname varchar(15) NOT NULL,
+                father_name varchar(30),
+                mother_name varchar(30),
+                mobile char(10) NOT NULL,
+                email varchar(50),
+                date_of_birth date NOT NULL,
+                class varchar(5) NOT NULL,
+                section varchar(2) NOT NULL,
+                address varchar(50) NOT NULL,
+                pin varchar(10) NOT NULL,
+                fees integer,
+                fees_paid integer);''')
+
+if __name__ == "__main__":
+    check_database_existance()
+    check_table_existance()
+    password = ""
